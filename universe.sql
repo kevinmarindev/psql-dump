ed4ad1dc63f962b05b07335ab71eed4c566ddb83
kevinmarindev <kevin.marin7@hotmail.com>
2025-02-09 23:36:38 -0600

    minor changes to file

--- universe.sql
+++ universe.sql
@@ -2,13 +2,35 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 17.2 (Debian 17.2-1.pgdg120+1)
--- Dumped by pg_dump version 17.2 (Debian 17.2-1.pgdg120+1)
+-- Dumped from database version 12.17 (Ubuntu 12.17-1.pgdg22.04+1)
+-- Dumped by pg_dump version 12.17 (Ubuntu 12.17-1.pgdg22.04+1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
-SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP DATABASE universe;
+--
+-- Name: universe; Type: DATABASE; Schema: -; Owner: freecodecamp
+--
+
+CREATE DATABASE universe WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';
+
+
+ALTER DATABASE universe OWNER TO freecodecamp;
+
+\connect universe
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
@@ -26,11 +48,12 @@ SET default_table_access_method = heap;
 --
 
 CREATE TABLE public.constellations (
-    id integer NOT NULL,
+    constellations_id integer NOT NULL,
     name character varying(100) NOT NULL,
     galaxy_id integer,
     is_visible boolean,
-    is_radioactive boolean
+    is_radioactive boolean,
+    description text
 );
 
 
@@ -49,13 +72,13 @@ CREATE SEQUENCE public.constellations_id_seq
     CACHE 1;
 
 
-ALTER SEQUENCE public.constellations_id_seq OWNER TO freecodecamp;
+ALTER TABLE public.constellations_id_seq OWNER TO freecodecamp;
 
 --
 -- Name: constellations_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
 --
 
-ALTER SEQUENCE public.constellations_id_seq OWNED BY public.constellations.id;
+ALTER SEQUENCE public.constellations_id_seq OWNED BY public.constellations.constellations_id;
 
 
 --
@@ -86,7 +109,7 @@ CREATE SEQUENCE public.galaxy_id_seq
     CACHE 1;
 
 
-ALTER SEQUENCE public.galaxy_id_seq OWNER TO freecodecamp;
+ALTER TABLE public.galaxy_id_seq OWNER TO freecodecamp;
 
 --
 -- Name: galaxy_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
@@ -124,7 +147,7 @@ CREATE SEQUENCE public.moon_id_seq
     CACHE 1;
 
 
-ALTER SEQUENCE public.moon_id_seq OWNER TO freecodecamp;
+ALTER TABLE public.moon_id_seq OWNER TO freecodecamp;
 
 --
 -- Name: moon_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
@@ -162,7 +185,7 @@ CREATE SEQUENCE public.planet_id_seq
     CACHE 1;
 
 
-ALTER SEQUENCE public.planet_id_seq OWNER TO freecodecamp;
+ALTER TABLE public.planet_id_seq OWNER TO freecodecamp;
 
 --
 -- Name: planet_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
@@ -200,7 +223,7 @@ CREATE SEQUENCE public.star_id_seq
     CACHE 1;
 
 
-ALTER SEQUENCE public.star_id_seq OWNER TO freecodecamp;
+ALTER TABLE public.star_id_seq OWNER TO freecodecamp;
 
 --
 -- Name: star_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
@@ -210,10 +233,10 @@ ALTER SEQUENCE public.star_id_seq OWNED BY public.star.star_id;
 
 
 --
--- Name: constellations id; Type: DEFAULT; Schema: public; Owner: freecodecamp
+-- Name: constellations constellations_id; Type: DEFAULT; Schema: public; Owner: freecodecamp
 --
 
-ALTER TABLE ONLY public.constellations ALTER COLUMN id SET DEFAULT nextval('public.constellations_id_seq'::regclass);
+ALTER TABLE ONLY public.constellations ALTER COLUMN constellations_id SET DEFAULT nextval('public.constellations_id_seq'::regclass);
 
 
 --
@@ -248,89 +271,79 @@ ALTER TABLE ONLY public.star ALTER COLUMN star_id SET DEFAULT nextval('public.st
 -- Data for Name: constellations; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-COPY public.constellations (id, name, galaxy_id, is_visible, is_radioactive) FROM stdin;
-1	weird	1	f	t
-2	weirds	2	f	t
-3	weirdxx	1	f	t
-\.
+INSERT INTO public.constellations VALUES (1, 'weird', 1, false, true, NULL);
+INSERT INTO public.constellations VALUES (2, 'weirds', 2, false, true, NULL);
+INSERT INTO public.constellations VALUES (3, 'weirdxx', 1, false, true, NULL);
 
 
 --
 -- Data for Name: galaxy; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-COPY public.galaxy (galaxy_id, name, age, years_away, oxygen_percentage) FROM stdin;
-1	Milky Way	1000000000	0	0.00
-2	Andromeda	800000000	250000	0.00
-3	Triangulum	900000000	300000	0.00
-4	Sombrero	1100000000	290000	0.00
-5	Whirlpool	1000000000	300000	0.00
-6	Pinwheel	950000000	280000	0.00
-\.
+INSERT INTO public.galaxy VALUES (1, 'Milky Way', 1000000000, 0, 0.00);
+INSERT INTO public.galaxy VALUES (2, 'Andromeda', 800000000, 250000, 0.00);
+INSERT INTO public.galaxy VALUES (3, 'Triangulum', 900000000, 300000, 0.00);
+INSERT INTO public.galaxy VALUES (4, 'Sombrero', 1100000000, 290000, 0.00);
+INSERT INTO public.galaxy VALUES (5, 'Whirlpool', 1000000000, 300000, 0.00);
+INSERT INTO public.galaxy VALUES (6, 'Pinwheel', 950000000, 280000, 0.00);
 
 
 --
 -- Data for Name: moon; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-COPY public.moon (moon_id, name, age, years_away, oxygen_percentage, planet_id) FROM stdin;
-1	moon	100	1	1.00	3
-2	Titan	200	5	1.20	3
-3	Europa	150	3	0.80	3
-4	Io	250	4	0.50	3
-5	Callisto	300	6	0.90	3
-6	Ganymede	400	7	1.10	3
-7	Enceladus	80	2	1.30	3
-8	Mimas	70	8	0.70	3
-9	Rhea	500	5	0.40	3
-10	Triton	300	9	0.60	3
-11	Hyperion	100	10	0.30	3
-12	Dione	200	11	0.20	3
-13	Phoebe	250	12	0.10	3
-14	Thebe	60	13	0.80	3
-15	Metis	90	14	1.00	3
-16	Pandora	110	15	0.90	3
-17	Prometheus	130	16	1.40	3
-18	Miranda	80	17	1.50	3
-19	Umbriel	140	18	1.60	3
-20	Ariel	220	19	0.95	3
-\.
+INSERT INTO public.moon VALUES (1, 'moon', 100, 1, 1.00, 3);
+INSERT INTO public.moon VALUES (2, 'Titan', 200, 5, 1.20, 3);
+INSERT INTO public.moon VALUES (3, 'Europa', 150, 3, 0.80, 3);
+INSERT INTO public.moon VALUES (4, 'Io', 250, 4, 0.50, 3);
+INSERT INTO public.moon VALUES (5, 'Callisto', 300, 6, 0.90, 3);
+INSERT INTO public.moon VALUES (6, 'Ganymede', 400, 7, 1.10, 3);
+INSERT INTO public.moon VALUES (7, 'Enceladus', 80, 2, 1.30, 3);
+INSERT INTO public.moon VALUES (8, 'Mimas', 70, 8, 0.70, 3);
+INSERT INTO public.moon VALUES (9, 'Rhea', 500, 5, 0.40, 3);
+INSERT INTO public.moon VALUES (10, 'Triton', 300, 9, 0.60, 3);
+INSERT INTO public.moon VALUES (11, 'Hyperion', 100, 10, 0.30, 3);
+INSERT INTO public.moon VALUES (12, 'Dione', 200, 11, 0.20, 3);
+INSERT INTO public.moon VALUES (13, 'Phoebe', 250, 12, 0.10, 3);
+INSERT INTO public.moon VALUES (14, 'Thebe', 60, 13, 0.80, 3);
+INSERT INTO public.moon VALUES (15, 'Metis', 90, 14, 1.00, 3);
+INSERT INTO public.moon VALUES (16, 'Pandora', 110, 15, 0.90, 3);
+INSERT INTO public.moon VALUES (17, 'Prometheus', 130, 16, 1.40, 3);
+INSERT INTO public.moon VALUES (18, 'Miranda', 80, 17, 1.50, 3);
+INSERT INTO public.moon VALUES (19, 'Umbriel', 140, 18, 1.60, 3);
+INSERT INTO public.moon VALUES (20, 'Ariel', 220, 19, 0.95, 3);
 
 
 --
 -- Data for Name: planet; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-COPY public.planet (planet_id, name, age, years_away, oxygen_percentage, star_id) FROM stdin;
-1	earth	1000000	0	50.20	1
-2	Mars	4500000	225	21.00	1
-3	Venus	4600000	42	0.00	1
-4	Jupiter	4600000	365	0.00	1
-5	Saturn	4500000	800	0.00	1
-6	Uranus	4500000	1700	0.00	1
-7	Neptune	4500000	2700	0.00	1
-8	Mercury	4600000	58	0.00	1
-9	Pluto	4500000	5900	0.00	1
-10	Earth	1000000	0	21.00	1
-11	Kepler-452b	5000000	1400	20.00	1
-12	Proxima Centauri b	4000000	4	20.50	1
-13	Alpha Centauri Bb	3500000	4	18.00	1
-\.
+INSERT INTO public.planet VALUES (1, 'earth', 1000000, 0, 50.20, 1);
+INSERT INTO public.planet VALUES (2, 'Mars', 4500000, 225, 21.00, 1);
+INSERT INTO public.planet VALUES (3, 'Venus', 4600000, 42, 0.00, 1);
+INSERT INTO public.planet VALUES (4, 'Jupiter', 4600000, 365, 0.00, 1);
+INSERT INTO public.planet VALUES (5, 'Saturn', 4500000, 800, 0.00, 1);
+INSERT INTO public.planet VALUES (6, 'Uranus', 4500000, 1700, 0.00, 1);
+INSERT INTO public.planet VALUES (7, 'Neptune', 4500000, 2700, 0.00, 1);
+INSERT INTO public.planet VALUES (8, 'Mercury', 4600000, 58, 0.00, 1);
+INSERT INTO public.planet VALUES (9, 'Pluto', 4500000, 5900, 0.00, 1);
+INSERT INTO public.planet VALUES (10, 'Earth', 1000000, 0, 21.00, 1);
+INSERT INTO public.planet VALUES (11, 'Kepler-452b', 5000000, 1400, 20.00, 1);
+INSERT INTO public.planet VALUES (12, 'Proxima Centauri b', 4000000, 4, 20.50, 1);
+INSERT INTO public.planet VALUES (13, 'Alpha Centauri Bb', 3500000, 4, 18.00, 1);
 
 
 --
 -- Data for Name: star; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-COPY public.star (star_id, name, age, years_away, oxygen_percentage, galaxy_id) FROM stdin;
-1	Betelgeuse	800000000	643	20.00	2
-2	Rigel	1200000000	860	20.50	2
-3	Sirius	1250000000	9	19.20	2
-6	Altair	200	100	21.30	2
-7	Arcturus	200	100	19.80	2
-8	Vega	200	100	20.70	2
-9	Capella	200	100	18.90	2
-\.
+INSERT INTO public.star VALUES (1, 'Betelgeuse', 800000000, 643, 20.00, 2);
+INSERT INTO public.star VALUES (2, 'Rigel', 1200000000, 860, 20.50, 2);
+INSERT INTO public.star VALUES (3, 'Sirius', 1250000000, 9, 19.20, 2);
+INSERT INTO public.star VALUES (6, 'Altair', 200, 100, 21.30, 2);
+INSERT INTO public.star VALUES (7, 'Arcturus', 200, 100, 19.80, 2);
+INSERT INTO public.star VALUES (8, 'Vega', 200, 100, 20.70, 2);
+INSERT INTO public.star VALUES (9, 'Capella', 200, 100, 18.90, 2);
 
 
 --
@@ -373,7 +386,7 @@ SELECT pg_catalog.setval('public.star_id_seq', 9, true);
 --
 
 ALTER TABLE ONLY public.constellations
-    ADD CONSTRAINT constellations_pkey PRIMARY KEY (id);
+    ADD CONSTRAINT constellations_pkey PRIMARY KEY (constellations_id);
 
 
 --
@@ -406,6 +419,14 @@ ALTER TABLE ONLY public.planet
 
 ALTER TABLE ONLY public.star
     ADD CONSTRAINT start_pkey PRIMARY KEY (star_id);
+
+
+--
+-- Name: constellations unique_desc; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.constellations
+    ADD CONSTRAINT unique_desc UNIQUE (description);
 
 
 --
@@ -475,4 +496,3 @@ ALTER TABLE ONLY public.star
 --
 -- PostgreSQL database dump complete
 --
-
